fc7751977c796fea97fcfcd80634732c4bbc61fe
granityo <frhnspensa@gmail.com>
2024-10-13 23:42:12 +0700

    Menambahkan fungsi ContohList untuk contoh penggunaan List dan MutableList di Kotlin.

--- app/src/main/java/com/example/praktikum1/collactions.kt
+++ app/src/main/java/com/example/praktikum1/collactions.kt
@@ -36,63 +36,8 @@ fun ContohList(){
 
 }
 
-fun  ContohSet(){
-    println()
-    println("=== Set ===")
-
-    // Set read-only
-    val readOnlyAbjad = setOf("A", "B", "C")
-    println(readOnlyAbjad)
-
-    val shape: MutableSet<String> = mutableSetOf("Circle","Square","Triangle")
-    println(shape)
-
-    //menambahkan data ke dalam set mutable
-    shape.add("Rectangle")
-
-    //menghapus data dari set mutable
-    shape.remove("Circle")
-    println(shape)
-
-    //set read-only
-    val shapeLocked: Set<String> = shape
-    println(shapeLocked)
-}
-
-fun ContohMap(){
-
-    println()
-    println("=== Map ===")
-    //map read-only
-    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to  3)
-    println(readOnlyShape)
-
-    //map mutable
-    val shape: MutableMap<String,Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to  3)
-    println(shape)
-
-    //menambahlan data ke dalam map mutable
-    shape["Rectangle"] = 4
-    println(shape)
-
-    //menghapus data dari map mutable
-    shape.remove("Circle")
-    println(shape)
-
-    //mengubah data dalam map mutable
-    shape["Square"] = 5
-    println(shape)
-
-    // map read-only
-    val shapesLocked: Map<String, Int> = shape
-    println(shapesLocked)
-
-    fun main(){
-        ContohList()
-        ContohSet()
-        ContohMap()
-    }
 
 
-}
+
+
 
