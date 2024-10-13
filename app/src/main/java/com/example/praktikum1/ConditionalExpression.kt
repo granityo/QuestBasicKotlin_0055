cffc9a0f193d0e6ec6d59c01a598fbf71de09c92
granityo <frhnspensa@gmail.com>
2024-10-14 00:03:47 +0700

    Menambahkan contoh penggunaan when dan try-catch di Kotlin.

--- app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
+++ app/src/main/java/com/example/praktikum1/ConditionalExpression.kt
@@ -11,4 +11,25 @@ fun ConditionalStatement(){
         println("NEgative Number")
     }
 
+    // when
+    val day =1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+    }
+
+    //Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+
 }
