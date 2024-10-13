ebff71beacc5ec1d19838cd12b36ec02df93e9e2
granityo <frhnspensa@gmail.com>
2024-10-13 23:47:41 +0700

    Menambahkan fungsi nullSafety untuk menunjukkan null safety di Kotlin dan penggunaan nullable types.

--- app/src/main/java/com/example/praktikum1/NullSafety.kt
+++ app/src/main/java/com/example/praktikum1/NullSafety.kt
@@ -1,29 +1,15 @@
 package com.example.praktikum1
 
-fun nullSafety(){
+fun nullSafety() {
     var neverNull: String = "This can't be null"
 
     var nullable: String? = "You can keep a Null here"
 
     nullable = null
 
-    if(neverNull == null){
+    if (neverNull == null) {
         println("inferredNonNull is Null")
     } else {
         println("inferredNonNull is not Null")
     }
-
-    println(neverNull. length)
-    println(nullable?.length)
-
-    fun strLeghth(notNull: String): Int{
-        return notNull.length
-    }
-
-    println(strLeghth(neverNull))
-
-}
-
-fun main(){
-    nullSafety()
 }
